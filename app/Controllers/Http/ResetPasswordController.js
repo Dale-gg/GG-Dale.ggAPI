390b4656a859622f92d125806eb346abf18ae4ba
João Lenon <lenonsec7@gmail.com>
2020-02-18 12:48:19 -0300

    feat: Transaction of Express to AdonisJS

--- app/Controllers/Http/ResetPasswordController.js
+++ app/Controllers/Http/ResetPasswordController.js
@@ -4,6 +4,8 @@ const { parseISO, isBefore, subHours } = require('date-fns');
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Token = use('App/Models/Token');
 
+const Antl = use('Antl');
+
 class ResetPasswordController {
   async store({ request, response }) {
     const { token, password } = request.only(['token', 'password']);
@@ -11,9 +13,10 @@ class ResetPasswordController {
     const userToken = await Token.findByOrFail('token', token);
 
     if (isBefore(parseISO(userToken.created_at), subHours(new Date(), 2))) {
-      return response
-        .status(400)
-        .json({ error: 'Invalid date range, please try again.' });
+      return response.status(400).json({
+        type: 'error-time-token',
+        msg: Antl.formatMessage('response.error-time-token'),
+      });
     }
 
     const user = await userToken.user().fetch();
@@ -21,6 +24,11 @@ class ResetPasswordController {
     user.password = password;
 
     await user.save();
+
+    return response.status(204).json({
+      type: 'password-reset',
+      msg: Antl.formatMessage('response.password-reset'),
+    });
   }
 }
 
