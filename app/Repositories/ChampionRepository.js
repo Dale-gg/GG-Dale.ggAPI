be3f909bdc385174447c636774dde645fd34628e
João Lenon <lenonSec7@hotmail.com>
2020-03-07 17:56:12 -0300

    feat: ChampionRepository

---
+++ app/Repositories/ChampionRepository.js
@@ -0,0 +1,27 @@
+/* eslint-disable camelcase */
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Champion = use('App/Models/Champion');
+
+class ChampionRepository {
+  async store(championAPI, gamePatch) {
+    const image_full = `http://ddragon.leagueoflegends.com/cdn/${gamePatch}/img/champion/${championAPI.image.full}`;
+    const image_splash = `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${championAPI.name}_0.jpg`;
+    const image_loading = `http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${championAPI.name}_0.jpg`;
+
+    const champion = await Champion.create({
+      key: championAPI.key,
+      name: championAPI.name,
+      title: championAPI.title,
+      tags: championAPI.tags,
+      image_full_url: image_full,
+      image_splash_url: image_splash,
+      image_loading_url: image_loading,
+      image_sprite_url: championAPI.image.sprite,
+      version: gamePatch,
+    });
+
+    return champion;
+  }
+}
+
+module.exports = ChampionRepository;
