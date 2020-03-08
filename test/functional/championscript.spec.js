c7456aa0af851d3f1570a53ff6342d1818c150ec
João Lenon <lenonSec7@hotmail.com>
2020-03-08 19:59:05 -0300

    feat: Tests refactor

--- test/functional/championscript.spec.js
+++ test/functional/championscript.spec.js
@@ -127,7 +127,5 @@ test('it should update all of the league of legends champions', async ({
 
   response.assertStatus(200);
 
-  console.log(response.body.champions)
-
   assert.exists(response.body.champions);
 }).timeout(99999);
