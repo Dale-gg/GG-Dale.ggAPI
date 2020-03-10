888e07c2b77bc3f62f369d907a579bb118fbe039
João Lenon <lenonsec7@gmail.com>
2020-03-10 16:39:06 -0300

    feat: Spells Routes

--- start/routes.js
+++ start/routes.js
@@ -52,8 +52,13 @@ Route.put(
   'ChampionController.updateAll'
 );
 
-// Spells Script
+// Spells
 Route.get('/spells/index', 'SpellController.index');
+Route.get('/spells/:spellName/show', 'SpellController.show');
+
+// Spell Script
+Route.post('/spells/:language/:version/storeAll', 'SpellController.storeAll');
+Route.put('/spells/:language/:version/updateAll', 'SpellController.updateAll');
 
 // JWT Middleware
 Route.group(() => {
