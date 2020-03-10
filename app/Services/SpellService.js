b09ad3b24e3bca18ba95eaa413af46df22bf8b77
João Lenon <lenonSec7@hotmail.com>
2020-03-10 18:14:17 -0300

    feat: SpellService

--- app/Services/SpellService.js
+++ app/Services/SpellService.js
@@ -7,30 +7,79 @@ const SpellRepository = use('App/Services/SpellRepository');
 
 const getAllSpells = require('../Utils/RiotAPI/getAllSpells');
 
+const getSpell = require('../Utils/RiotAPI/getSpell');
+
 class SpellService {
   constructor() {
     this.spellRepository = new SpellRepository();
   }
 
   async index() {
-    const champions = await this.championRepository.index();
+    const champions = await this.spellRepository.index();
 
     return champions;
   }
 
   async store({ gamePatch, language, spellName }) {
+    const spellAPI = await getSpell(gamePatch, language, spellName);
+
+    if (spellAPI.id == null || spellAPI.id === 'Error') {
+      return null;
+    }
+
+    await this.spellRepository.store(spellAPI, gamePatch);
+
+    const resSpell = await Spell.findBy({ name: spellName });
+
+    return resSpell;
   }
 
   async show(spellName) {
+    const spell = await this.spellRepository.show(spellName);
+
+    return spell;
   }
 
   async update(spellName, { gamePatch, language }) {
+    const spellAPI = await getSpell(gamePatch, language, spellName);
+
+    if (spellAPI.id == null || spellAPI.id === 'Error') {
+      return null;
+    }
+
+    await this.spellRepository.update(spellName, spellAPI, gamePatch);
+
+    const resSpell = await Spell.findBy({ name: spellName });
+
+    return resSpell;
   }
 
   async storeAll({ version, language }) {
+    const spellsAPI = await getAllSpells(version, language);
+
+    const promises = [];
+    for (const spell in spellsAPI) {
+      promises.push(this.spellRepository.storeAll(spellsAPI[spell], version));
+    }
+    await Promise.all(promises);
+
+    const resSpells = await Spell.all();
+
+    return resSpells;
   }
 
   async updateAll({ version, language }) {
+    const spellsAPI = await getAllSpells(version, language);
+
+    const promises = [];
+    for (const spell in spellsAPI) {
+      promises.push(this.spellRepository.updateAll(spellsAPI[spell], version));
+    }
+    await Promise.all(promises);
+
+    const resSpells = await Spell.all();
+
+    return resSpells;
   }
 }
 
