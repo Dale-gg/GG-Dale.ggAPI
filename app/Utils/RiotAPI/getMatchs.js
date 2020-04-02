fbbf9ad283a64dfe5b12c64f907ff1938e19c9ea
João Lenon <lenonsec7@gmail.com>
2020-04-01 22:10:44 -0300

    feat: Modified getMatchs for Development and Delet

--- app/Utils/RiotAPI/getMatchs.js
+++ app/Utils/RiotAPI/getMatchs.js
@@ -1,11 +1,11 @@
 /* eslint-disable guard-for-in */
-const getMatchlistUrl = '.api.riotgames.com/lol/match/v4/matchlists/by-account/';
-const getMatchDto = '.api.riotgames.com/lol/match/v4/matches/';
+const getMatchlistUrl =
+  '.api.riotgames.com/lol/match/v4/matchlists/by-account/';
 
 const Env = use('Env');
 const Axios = use('axios');
 
-async function getMatchs(region, accountId) {
+async function getMatchs(accountId, region) {
   try {
     const apiResponse = await Axios.get(
       `https://${region}${getMatchlistUrl}${accountId}${Env.get(
