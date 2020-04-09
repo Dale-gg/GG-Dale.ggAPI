194a3d470dc1957390ca50e8241539d7686404a1
João Lenon <lenonsec7@gmail.com>
2020-04-08 21:12:19 -0300

    feat: Redis connection bull

---
+++ config/bull.js
@@ -0,0 +1,20 @@
+/* eslint-disable strict */
+
+'use strict';
+
+const Env = use('Env');
+
+module.exports = {
+  // redis connection
+  connection: Env.get('BULL_CONNECTION', 'bull'),
+  bull: {
+    redis: {
+      host: '127.0.0.1',
+      port: 6379,
+      password: null,
+      db: 0,
+      keyPrefix: '',
+    },
+  },
+  remote: 'redis://redis.example.com?password=correcthorsebatterystaple',
+};
