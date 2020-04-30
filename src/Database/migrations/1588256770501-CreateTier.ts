f258b7ff1898f7b73a8ae183af705be8ddb0b0e0
João Lenon <lenonsec7@gmail.com>
2020-04-30 20:26:15 -0300

    feat: Tier Migration

---
+++ src/Database/migrations/1588256770501-CreateTier.ts
@@ -0,0 +1,86 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateTier1588256770501 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'summoners',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'league_id',
+            type: 'varchar',
+          },
+          {
+            name: 'queue_type',
+            type: 'varchar',
+          },
+          {
+            name: 'tier',
+            type: 'varchar',
+          },
+          {
+            name: 'rank',
+            type: 'varchar',
+          },
+          {
+            name: 'pdl',
+            type: 'integer',
+          },
+          {
+            name: 'winrate',
+            type: 'varchar',
+          },
+          {
+            name: 'wins',
+            type: 'integer',
+          },
+          {
+            name: 'losses',
+            type: 'integer',
+          },
+          {
+            name: 'inactive',
+            type: 'boolean',
+          },
+          {
+            name: 'veteran',
+            type: 'boolean',
+          },
+          {
+            name: 'hot_streak',
+            type: 'boolean',
+          },
+          {
+            name: 'fresh_blood',
+            type: 'boolean',
+          },
+          {
+            name: 'season',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('summoners');
+  }
+}
