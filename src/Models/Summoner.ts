ddb43dbde262a278a6e96c3f2fd15f0e31e390f5
João Lenon <lenonsec7@gmail.com>
2020-04-30 20:27:20 -0300

    feat: Summoner Model

---
+++ src/Models/Summoner.ts
@@ -0,0 +1,41 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('summoners')
+class Summoner {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  account_id: string;
+
+  @Column()
+  summoner_id: string;
+
+  @Column()
+  puuid: string
+
+  @Column()
+  summoner_name: string
+
+  @Column()
+  region: string
+
+  @Column()
+  revision_date: Date
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  // Relations -> Tiers, Matchs
+}
+
+export default Summoner;
