aa1ede76015d1a1776b9a58bcf3d360d16563e41
João Lenon <lenonsec7@gmail.com>
2020-04-30 20:25:42 -0300

    feat: SummonerController

---
+++ src/Controllers/Summoners/SummonerController.ts
@@ -0,0 +1,9 @@
+class SummonerController {
+  public async store(): Promise<void> {}
+
+  public async show(): Promise<void> {}
+
+  public async update(): Promise<void> {}
+}
+
+export default SummonerController;
