8b36bbe9c0790ce9758831d567bf71f1b52a795d
João Lenon <lenonsec7@gmail.com>
2020-05-06 11:08:36 -0300

    feat: Refactor to run migrations

---
+++ src/Database/migrations/1588730365287-CreateSpell.ts
@@ -0,0 +1,68 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export default class CreateSpell1588730365287 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.createTable(
+        new Table({
+          name: 'spells',
+          columns: [
+            {
+              name: 'id',
+              type: 'uuid',
+              isPrimary: true,
+              generationStrategy: 'uuid',
+              default: 'uuid_generate_v4()',
+            },
+            {
+              name: 'name',
+              type: 'varchar',
+            },
+            {
+              name: 'key',
+              type: 'integer',
+            },
+            {
+              name: 'spell_dd',
+              type: 'varchar',
+            },
+            {
+              name: 'description',
+              type: 'varchar',
+            },
+            {
+              name: 'group',
+              type: 'varchar',
+            },
+            {
+              name: 'modes',
+              type: 'varchar',
+            },
+            {
+              name: 'image_full_url',
+              type: 'varchar',
+            },
+            {
+              name: 'image_sprite_url',
+              type: 'varchar',
+            },
+            {
+              name: 'created_at',
+              type: 'timestamp',
+              default: 'now()',
+            },
+            {
+              name: 'updated_at',
+              type: 'timestamp',
+              default: 'now()',
+            },
+          ],
+        }),
+      );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropTable('participants');
+    }
+
+}
