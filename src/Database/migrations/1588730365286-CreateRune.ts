8679bd944d5500589c71fad8567a124acfbcd291
João Lenon <lenonsec7@gmail.com>
2020-05-06 11:08:34 -0300

    feat: Refactor to run migrations

---
+++ src/Database/migrations/1588730365286-CreateRune.ts
@@ -0,0 +1,77 @@
+import {MigrationInterface, QueryRunner, Table, TableForeignKey} from "typeorm";
+
+export default class CreateRune1588730365286 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.createTable(
+        new Table({
+          name: 'runes',
+          columns: [
+            {
+              name: 'id',
+              type: 'uuid',
+              isPrimary: true,
+              generationStrategy: 'uuid',
+              default: 'uuid_generate_v4()',
+            },
+            {
+              name: 'id_api',
+              type: 'integer',
+            },
+            {
+              name: 'tree_id',
+              type: 'uuid',
+              isNullable: true,
+            },
+            {
+              name: 'key',
+              type: 'integer',
+            },
+            {
+              name: 'icon',
+              type: 'varchar',
+            },
+            {
+              name: 'name',
+              type: 'varchar',
+            },
+            {
+              name: 'shortDesc',
+              type: 'varchar',
+            },
+            {
+              name: 'longDesc',
+              type: 'varchar',
+            },
+            {
+              name: 'created_at',
+              type: 'timestamp',
+              default: 'now()',
+            },
+            {
+              name: 'updated_at',
+              type: 'timestamp',
+              default: 'now()',
+            },
+          ],
+        }),
+      );
+
+      await queryRunner.createForeignKey(
+        'runes',
+        new TableForeignKey({
+          columnNames: ['tree_id'],
+          referencedColumnNames: ['id'],
+          referencedTableName: 'trees',
+          name: 'RunesTress',
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE',
+        }),
+      );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropTable('runes');
+    }
+
+}
