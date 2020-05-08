dfec9de2d0abec3f2d6f28e2bb64103f6fda1e60
João Lenon <lenonsec7@gmail.com>
2020-05-07 22:54:22 -0300

    feat: implemented middleware in post route

--- src/Routes/summoners.routes.ts
+++ src/Routes/summoners.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express'
 
 import SummonerController from '../Controllers/Summoners/SummonerController'
+import SummonerFounder from '../Middlewares/SummonerFounder'
 
 const summonerRouter = Router()
 const Controller = new SummonerController()
+const Founder = new SummonerFounder()
 
-summonerRouter.post('/:name', Controller.store)
-summonerRouter.get('/:name', Controller.show)
-summonerRouter.put('/:name', Controller.update)
+summonerRouter.get('/', Controller.show)
+summonerRouter.post('/', Founder.handle, Controller.store)
+summonerRouter.put('/', Controller.update)
 
 export default summonerRouter
