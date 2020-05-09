0e17fe80508eeab80c23cc70ff52d6a4ae0ee3a7
João Lenon <lenonsec7@gmail.com>
2020-05-08 23:18:34 -0300

    feat: need refactor

--- src/Services/CreateSummonerService.ts
+++ src/Services/CreateSummonerService.ts
@@ -1,17 +1,21 @@
 import { getRepository } from "typeorm"
 import Summoner from "../Models/Summoner"
+import { LolApi } from '@jlenon7/zedjs/dist'
 
 interface IRequest {
-  region?: string
-  summonerName?: string
+  region: any
+  summonerName: string
 }
 
 class CreateSummonerService {
   public async execute({ region, summonerName }: IRequest): Promise<Summoner> {
+    const api = new LolApi()
     const repository = getRepository(Summoner)
 
+    const S = await api.Summoner.getByName(summonerName, region)
+
     const summoner = repository.create({
-      account_id: '1231321321',
+      account_id: S.accountId,
       summoner_id: '123132321321',
       puuid: '95256456-9077-11ea-bb37-0242ac130002',
       revision_date: '213213231321',
