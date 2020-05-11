b33b1a87d01e56390cebab4e6e0bcf938f9fe2bf
João Lenon <lenonsec7@gmail.com>
2020-05-11 13:17:36 -0300

    feat: ChampionObject Interface

--- src/@types/IChampion.ts
+++ src/@types/IChampion.ts
@@ -9,3 +9,18 @@ export default interface IChampion {
   image_splash_url?: string
   image_sprite_url?: string
 }
+
+export interface IChampionObject {
+  id: string
+  name: string
+  key: string
+  title: string
+  tags: string[]
+  version: string
+  image_full_url: string
+  image_splash_url: string
+  image_loading_url: string
+  image_sprite_url: string
+  created_at: Date
+  updated_at: Date
+}
