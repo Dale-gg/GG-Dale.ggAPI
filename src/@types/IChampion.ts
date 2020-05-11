106b9608d78ad4e09367a6964b86ea8940b2e3a8
João Lenon <lenonsec7@gmail.com>
2020-05-11 13:07:07 -0300

    feat: Champion Interface

---
+++ src/@types/IChampion.ts
@@ -0,0 +1,11 @@
+export default interface IChampion {
+  name: string
+  key: string
+  title: string
+  tags: string[]
+  version: string
+  image_full_url?: string
+  image_loading_url?: string
+  image_splash_url?: string
+  image_sprite_url?: string
+}
