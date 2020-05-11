bd687346c0b5ef5527273a5597b5d9bd3f6a38cc
João Lenon <lenonsec7@gmail.com>
2020-05-11 13:09:16 -0300

    feat: Summoner and Champion Interface

--- src/Database/Factory.ts
+++ src/Database/Factory.ts
@@ -1,7 +1,9 @@
 import faker from 'faker'
 import { getRepository } from 'typeorm'
 
-import { IFactory, IChampion, ISummoner } from './Types'
+import IFactory from '../@types/IFactory'
+import ISummoner from '../@types/ISummoner'
+import IChampion from '../@types/IChampion'
 import Summoner from '../App/Models/Summoner'
 import Champion from '../App/Models/Champion'
 
@@ -15,7 +17,7 @@ export default class Factory implements IFactory {
       profile_icon: faker.random.number(),
       summoner_level: faker.random.number(),
       puuid: faker.random.uuid(),
-      revision_date: faker.date.recent(),
+      revision_date: faker.random.number(),
       region: data.region || 'br1',
       summoner_name: data.summonerName || 'iLenon7',
       ...data,
@@ -35,7 +37,7 @@ export default class Factory implements IFactory {
         account_id: faker.random.uuid(),
         summoner_id: faker.random.uuid(),
         puuid: faker.random.uuid(),
-        revision_date: faker.date.recent(),
+        revision_date: faker.random.number(),
         region: `${data.region}:${value}` || `br1:${value}`,
         summoner_name: `${data.summonerName}:${value}` || `'iLenon7:${value}'`,
         ...data,
