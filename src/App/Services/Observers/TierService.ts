b1b165a5ec7de468c8afe7a78e01f33702c2ac34
João Lenon <lenonsec7@gmail.com>
2020-05-12 23:56:00 -0300

    feat: Rename

---
+++ src/App/Services/Observers/TierService.ts
@@ -0,0 +1,17 @@
+import { IObserver, ISubject } from '../../../@types/IObserver'
+import { SummonerV4DTO } from '@jlenon7/zedjs/dist/models-dto'
+
+class TierService implements IObserver {
+  private subject: ISubject
+
+  constructor(summonerService: ISubject) {
+    this.subject = summonerService
+    summonerService.registerObserver(this)
+  }
+
+  updateSummoner(summoner: SummonerV4DTO): void {
+    console.log(`> [TIER] Summoner ${summoner.name}`)
+  }
+}
+
+export default TierService
