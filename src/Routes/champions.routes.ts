26393ab106d2603a43644d4c624d2d36142bfbd0
João Lenon <lenonsec7@gmail.com>
2020-05-14 10:35:19 -0300

    feat: validators on champions routes

--- src/Routes/champions.routes.ts
+++ src/Routes/champions.routes.ts
@@ -1,14 +1,16 @@
 import { Router } from 'express'
 
+import StoreValidate from '../App/Validators/Champion/StoreChampion'
+import UpdateValidate from '../App/Validators/Champion/UpdateChampion'
 import ChampionController from '../App/Controllers/DDragon/ChampionController'
 
 const routes = Router()
 
 const Controller = new ChampionController()
 
-routes.post('/', Controller.store)
-routes.put('/script/all', Controller.updateAll)
-routes.put('/:key', Controller.update)
 routes.post('/script/all', Controller.storeAll)
+routes.put('/script/all', Controller.updateAll)
+routes.post('/', StoreValidate, Controller.store)
+routes.put('/:key', UpdateValidate, Controller.update)
 
 export default routes
