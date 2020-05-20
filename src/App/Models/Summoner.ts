5975789393b8d3a6adae770dbe41d19036e4bde5
João Lenon <lenonsec7@gmail.com>
2020-05-20 12:39:03 -0300

    feat: Reverse of tier relation

--- src/App/Models/Summoner.ts
+++ src/App/Models/Summoner.ts
@@ -6,7 +6,9 @@ import {
   PrimaryGeneratedColumn,
   CreateDateColumn,
   UpdateDateColumn,
+  OneToMany,
 } from 'typeorm'
+import Tier from './Tier'
 
 @Entity('summoners')
 class Summoner implements ISummonerObject {
@@ -36,6 +38,9 @@ class Summoner implements ISummonerObject {
 
   @Column()
   revision_date: number
+
+  @OneToMany(_type => Tier, tier => tier.summoner)
+  tiers: Tier[]
 
   @CreateDateColumn()
   created_at: Date
