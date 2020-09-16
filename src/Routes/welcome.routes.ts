f1daef0e57b36637c0402907b59952fff1a8bf3e
João Lenon <lenonsec7@gmail.com>
2020-09-16 12:48:05 -0300

    feat: GG v1.0.2

--- src/Routes/welcome.routes.ts
+++ src/Routes/welcome.routes.ts
@@ -1,37 +1,14 @@
 import { Router, Request, Response } from 'express'
+import pjson from '../../package.json'
 
 const routes = Router()
 
 routes.get('/', (request: Request, response: Response) => {
   return response.json({
-    project: process.env.APP_NAME,
-    prefix: process.env.APP_PREFIX,
-    welcome: 'Welcome to Dale.gg DevAPI',
-    repository: {
-      openSource: 'This is a open source project by Dale.gg Organization!',
-      repository: 'https://github.com/Dale-gg/GG-Dale.ggAPI',
-    },
-    endpoints: {
-      whereFind: 'You can find all endpoints in our Github repository',
-      quickExample: {
-        Summoner: {
-          POST:
-            'https://dalegg-api.herokuapp.com/gg/v1/summoners?region=br1&summonerName=iLenon7',
-          GET:
-            'https://dalegg-api.herokuapp.com/gg/v1/summoners?region=br1&summonerName=iLenon7',
-        },
-      },
-    },
-    withLove: {
-      contributor1: {
-        nickname: 'jlenon7',
-        repository: 'https://github.com/jlenon7',
-      },
-      contributor2: {
-        nickname: 'Adryell',
-        repository: 'https://github.com/Adryell',
-      },
-    },
+    prefix: '/gg/v1',
+    domain: 'GG - Dale.gg',
+    version: `${pjson.version}`,
+    greeting: 'Welcome to Dale.gg API!',
   })
 })
 
